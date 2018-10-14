3ff8e45445c2e86dc58f528fa9ddd84bf56044d8
Unknown <stian.clausen@gmail.com>
2018-10-14 03:08:06 +0200

    learning controllers

---
+++ AspNetCoreVideo/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCoreVideo.Models;
+
+namespace AspNetCoreVideo.Controllers
+{
+	public class HomeController : Controller
+	{
+		IHostingEnvironment _enviroment;
+		public HomeController(IHostingEnvironment env)
+		{
+			_enviroment = env;
+		}
+
+		public ViewResult Index()
+		{
+			var model = new Video
+			{
+				Id = 1,
+				Title = "Shreck"
+			};
+			return View(model);
+		}
+
+	}
+}
